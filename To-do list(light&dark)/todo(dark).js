ad633ae19f16c41db295e2a511c8d60c4db5c23c
Ankita Dolai <78500268+Ankita1827@users.noreply.github.com>
2022-10-15 01:10:27 +0530

    Add files via upload

---
+++ To-do list(light&dark)/todo(dark).js
@@ -0,0 +1,6 @@
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+
+dark_mode_toggle.onclick = () => {
+    document.querySelector('body').classList.toggle('light')
+    document.querySelector('body').classList.toggle('dark')
+}
