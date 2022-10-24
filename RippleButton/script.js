9319593498c4b074cb2439b67c30dc8fcc035d47
hudadamar21 <hudadamar21@gmail.com>
2022-10-24 11:00:25 +0700

    add: Ripple Button

---
+++ RippleButton/script.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('a');
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+        setTimeout(() => {
+            ripples.remove()
+        }, 1000);
+    });
+});
